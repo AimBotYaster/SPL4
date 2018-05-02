14bfb50c88fce21600c07b3dab12ce463235c7ce
AimBotYaster <yaser.tara22@gmail.com>
2018-05-02 09:35:48 +0200

    Level 1 fertig

--- Parkgarage/src/Parkgarage_Level1.java
+++ Parkgarage/src/Parkgarage_Level1.java
@@ -3,9 +3,9 @@ public class Parkgarage_Level1 {
 
 	public static void main(String[] args) {
 
-		int parkpleatze = 10;
-		int Autos = 4;
-		String parken = "6 2 -6 3 4 -3 -4 -2";
+		int parkpleatze = 100;
+		int Autos = 20;
+		String parken = "19 -19 11 -11 1 -1 17 -17 10 7 -10 -7 3 14 -14 -3 13 -13 4 12 20 18 6 16 -18 -12 -16 5 -5 -20 -6 -4 8 15 -8 2 -15 9 -2 -9";
 		String[] parkvorgaenge = parken.split(" ");
 		int maxAutosInGarage = 0;
 		int autosInGarage = 0;
@@ -16,12 +16,21 @@ public class Parkgarage_Level1 {
 
 			if (auto < 0) {
 				System.out.println("Auto parkt aus...." + auto);
+				autosInGarage--;
 
 			} else {
 				System.out.println("Auto park ein..." + auto);
+				autosInGarage++;
 
 			}
+
+			if (maxAutosInGarage < autosInGarage) {
+				maxAutosInGarage = autosInGarage;
+
+			}
+
 		}
+		System.out.println("Es sind" +maxAutosInGarage);
 
 	}
 
